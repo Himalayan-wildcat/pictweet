f3313aa5888f650c1d4e1335cc8ce7126ad225cb
Himalayan-wildcat <35422041+Himalayan-wildcat@users.noreply.github.com>
2018-04-05 17:06:28 +0900

    Revert "created DB table for users"

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,5 @@ module Pictweet
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.generators do |g|
- 	  g.helper false
- 	  g.test_framework false
- 	  g.javascripts false
- 	end
   end
 end
