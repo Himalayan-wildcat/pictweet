5bbfdfc1df124dc291bd9b04dfa90ca5f343e1ef
Himalayan-wildcat <npng.h.0709@gmail.com>
2018-04-06 13:50:42 +0900

    created detailed page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -26,6 +26,11 @@ class TweetsController < ApplicationController
     tweet.update(tweet_params) if tweet.user_id == current_user.id
   end
 
+  def show
+    @tweet = Tweet.find(params[:id])
+    binding.pry
+  end
+
   private
   def tweet_params
     params.permit(:text, :image)
